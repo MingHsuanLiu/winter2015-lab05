7141010389d3151e5362f383dd621b4d8fd4b603
A00770012 <A00770012bcit@gmail.com>
2015-02-19 20:40:04 -0800

    included the error message div Signed-off-by:A00770012 <A00770012bcit@gmail.com>

--- application/views/quote_edit.php
+++ application/views/quote_edit.php
@@ -1,11 +1,13 @@
 <!--using formfields helper to style the form-->
 
 <div class="row">
-  <form action="/admin/confirm" method="post">
-    {fid}
-    {fwho}
-    {fmug}
-    {fwhat}
-    {fsubmit}
-  </form>
+    <!--error message formating-->
+    <div class="errors">{message}</div>
+    <form action="/admin/confirm" method="post">
+        {fid}
+        {fwho}
+        {fmug}
+        {fwhat}
+        {fsubmit}
+    </form>
 </div>
